3eeb55f14d8efb8da3392c9a37d231ac5ccbb2bc
Florence Cloutier <northeastflo@gmail.com>
2024-11-27 14:29:59 -0500

    Added model

---
+++ packages/drqv2.py
@@ -0,0 +1,101 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import distributions as pyd
+from torch.distributions.utils import _standard_normal
+
+class TruncatedNormal(pyd.Normal):
+    def __init__(self, loc, scale, low=-1.0, high=1.0, eps=1e-6):
+        super().__init__(loc, scale, validate_args=False)
+        self.low = low
+        self.high = high
+        self.eps = eps
+
+    def _clamp(self, x):
+        clamped_x = torch.clamp(x, self.low + self.eps, self.high - self.eps)
+        x = x - x.detach() + clamped_x.detach()
+        return x
+
+    def sample(self, clip=None, sample_shape=torch.Size()):
+        shape = self._extended_shape(sample_shape)
+        eps = _standard_normal(shape,
+                               dtype=self.loc.dtype,
+                               device=self.loc.device)
+        eps *= self.scale
+        if clip is not None:
+            eps = torch.clamp(eps, -clip, clip)
+        x = self.loc + eps
+        return self._clamp(x)
+
+
+class Encoder(nn.Module):
+    def __init__(self, obs_shape):
+        super().__init__()
+
+        assert len(obs_shape) == 3
+        self.repr_dim = 32 * 35 * 35
+
+        self.convnet = nn.Sequential(nn.Conv2d(obs_shape[0], 32, 3, stride=2),
+                                     nn.ReLU(), nn.Conv2d(32, 32, 3, stride=1),
+                                     nn.ReLU(), nn.Conv2d(32, 32, 3, stride=1),
+                                     nn.ReLU(), nn.Conv2d(32, 32, 3, stride=1),
+                                     nn.ReLU())
+
+    def forward(self, obs):
+        obs = obs / 255.0 - 0.5
+        h = self.convnet(obs)
+        h = h.view(h.shape[0], -1)
+        return h
+
+
+class Actor(nn.Module):
+    def __init__(self, repr_dim, action_shape, feature_dim, hidden_dim):
+        super().__init__()
+
+        self.trunk = nn.Sequential(nn.Linear(repr_dim, feature_dim),
+                                   nn.LayerNorm(feature_dim), nn.Tanh())
+
+        self.policy = nn.Sequential(nn.Linear(feature_dim, hidden_dim),
+                                    nn.ReLU(inplace=True),
+                                    nn.Linear(hidden_dim, hidden_dim),
+                                    nn.ReLU(inplace=True),
+                                    nn.Linear(hidden_dim, action_shape[0]))
+
+    def forward(self, obs, std):
+        h = self.trunk(obs)
+
+        mu = self.policy(h)
+        mu = torch.tanh(mu)
+        std = torch.ones_like(mu) * std
+
+        dist = TruncatedNormal(mu, std)
+        return dist
+
+class CombinedModel(nn.Module):
+    def __init__(self, obs_shape, action_shape, feature_dim, hidden_dim):
+        super().__init__()
+        self.encoder = Encoder(obs_shape)
+        self.actor = Actor(self.encoder.repr_dim, action_shape, feature_dim, hidden_dim)
+
+    def forward(self, obs, std):
+        encoded = self.encoder(obs)
+        mu, std = self.actor(encoded, std)
+        return mu, std
+    
+encoder_weights_path = "encoder_weights.pth"
+actor_weights_path = "actor_weights.pth"
+
+obs_shape = (9, 84, 84)
+action_shape = (2, )
+feature_dim = 50
+hidden_dim = 1024
+
+model = CombinedModel(obs_shape, action_shape, feature_dim, hidden_dim)
+
+model.encoder.load_state_dict(torch.load(encoder_weights_path))
+model.actor.load_state_dict(torch.load(actor_weights_path))
+
+model.eval()
+
+print("Done")
+
--- packages/model.py
+++ packages/model.py
@@ -1,20 +1,37 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import cv2
+import torch
 from duckietown_messages.actuators.differential_pwm import DifferentialPWM
+from packages.drqv2 import CombinedModel
 
+obs_shape = (9, 84, 84)
+action_shape = (2, )
+feature_dim = 50
+hidden_dim = 1024
+
+encoder_weights_path = "encoder_weights.pth"
+actor_weights_path = "actor_weights.pth"
 
 class PyTorchModel:
     def __init__(self):
         print("initializing PyTorch model")
+        self.model = CombinedModel(obs_shape, action_shape, feature_dim, hidden_dim)
+        self.model.encoder.load_state_dict(torch.load(encoder_weights_path))
+        self.model.actor.load_state_dict(torch.load(actor_weights_path))
+        self.img_queue = np.zeroes((3, 3, 84, 84))
 
 
     def get_wheel_velocities_from_image(self, img):
+        # Resize img to (3, 84, 84)
+        resized_img = cv2.resize(img, (3, 84, 84)) 
+        
+        self.img_queue = self.img_queue[1:, ...]
+        self.img_queue = np.append(self.img_queue, resized_img)
+        input = self.img_queue.reshape((-1, 84, 84))
+        
+        mu, _ = self.model(input, 1)
 
-        rads_left = 0.1
-        rads_right = 0.1
-        pwm_left = 0.4 * rads_left
-        pwm_right = 0.4 * rads_right
-
-        pwm = DifferentialPWM(left=pwm_left, right=pwm_right)
+        pwm = DifferentialPWM(left=mu[0], right=mu[1])
         return pwm
